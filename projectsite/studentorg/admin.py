92d2190a93c905aab54813179ec7e92959103a55
iAmRangz <superango2012@gmail.com>
2024-04-02 09:26:09 +0800

    refractor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -4,5 +4,20 @@ from .models import College, Program, Organization, Student, OrgMember
 admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'lastname', 'firstname', 'middlename', 'program')
+    search_fields = ('student_id', 'lastname', 'firstname')
+
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ('student', 'get_member_program', 'organization', 'date_joined',)
+    search_fields = ('student__student_id', 'organization__name')
+    
+    def get_member_program(self, obj):
+        try:
+            member = Student.objects.get(student_id=obj.student.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None
